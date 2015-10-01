dbc4549faa3a89a67fd96ebd8c30bb8ba3ae5066
Mike Houghton <mike@Mikes-MacBook-Air.local>
2015-10-01 22:56:12 +0100

    in progress

--- src/Libs.hs
+++ src/Libs.hs
@@ -1,91 +1,101 @@
--- module Libs where
+
+module Libs where
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults -fno-warn-unused-do-bind #-}
 import Text.ParserCombinators.Parsec
+import Net
+
 -- import Control.Applicative hiding ((<|>), optional, many)
 sep :: Char
 sep = ':'
 
 openBrace :: Char
 openBrace = '{'
+
 closeBrace :: Char
 closeBrace = '}'
 
-deps :: String
-deps = "deps"
-env :: String
-env = "env"
+depsToken :: String
+depsToken = "deps"
 
+envToken :: String
+envToken = "env"
 
-
-
-ws :: Parser String 
-ws = many (oneOf " ")
-
-int :: (Integral a, Read a) => Parser a
-int =  fmap read  (many1 digit)
-
--- stringLike :: Parser String
--- stringLike = char '"' *> many (noneOf ['\"', '\r', '\n']) <* char '"'
-
--- lexeme :: Parser a -> Parser a
--- lexeme p = ws *> p <* ws
 
 -- eg 'org.apache.felix:org.apache.felix.bundlerepository:2.0.6'
 data LibRef = LibRef {grp :: String, artifact :: String, version :: String} deriving (Show)
-
-
 type Deps = [LibRef]
+
+fName :: LibRef -> String
+fName l = (grp l) ++ "-" ++ (version l) ++ ".jar"
+-- ----------------------------------------------------------------------------
+
+int :: (Integral a, Read a) => Parser a
+int =  fmap read  (many1 digit)
+-- ----------------------------------------------------------------------------
 
 letterDigUndrDot :: Parser String
 letterDigUndrDot = do
    many1 (letter <|> digit <|> char '_' <|> char '.')
 
+-- ----------------------------------------------------------------------------
 
 libRefParser :: Parser LibRef
 libRefParser = do
-	spaces
-	char '"'
-	grp <- letterDigUndrDot
-	char sep
-	art <- letterDigUndrDot
-	char sep
-	ver <- letterDigUndrDot
-	char '"'
-	spaces
-	return $ LibRef grp art ver
-
+  spaces
+  char '"'
+  grp <- letterDigUndrDot
+  char sep
+  art <- letterDigUndrDot
+  char sep
+  ver <- letterDigUndrDot
+  char '"'
+  spaces
+  return $ LibRef grp art ver
+-- ----------------------------------------------------------------------------
 depsParser :: Parser Deps
 depsParser = do
-	spaces
-	string deps
-	char openBrace
-	spaces
-	manyDep <- many libRefParser
-	spaces
-	char closeBrace
-	return $ manyDep
+  spaces
+  string depsToken
+  char openBrace
+  spaces
+  manyDep <- many libRefParser
+  spaces
+  char closeBrace
+  return $ manyDep
+-- ----------------------------------------------------------------------------
 
 testLibRefParser = do
-	let  lref = parse libRefParser "Libref" "org.apache.felix:org.apache.felix.bundlerepository:2.0.6"
-	case lref of 
-		Left err  ->  show err
-		Right val ->  show val
+  let  lref = parse libRefParser "Libref" "org.apache.felix:org.apache.felix.bundlerepository:2.0.6"
+  case lref of 
+    Left err  ->  show err
+    Right val ->  show val
 
+-- ----------------------------------------------------------------------------
 
 testManyLibRefParser = do
-	let  lref = parse (many libRefParser) "Libref" ""
-	case lref of 
-		Left err  ->  show err
-		Right val ->  show val
+  let  lref = parse (many libRefParser) "Libref" ""
+  case lref of 
+    Left err  ->  show err
+    Right val ->  show val
+-- ----------------------------------------------------------------------------
 
 testDepsParser text = do 
-	let lref = parse depsParser "LibRef" text
-	case lref of 
-		Left err  ->  show err
-		Right val ->  show val
+  let lref = parse depsParser "LibRef" text
+  case lref of 
+    Left err  ->  show err
+    Right val ->  show val
+-- ----------------------------------------------------------------------------
+-- data LibRef = LibRef {grp :: String, artifact :: String, version :: String} deriving (Show)
+makeURL :: LibRef -> String
+makeURL l = "http://mvnrepository.com/artifact/" ++ (grp l) ++ "/" ++ (artifact l) ++ "/" ++ (version l) ++ "/" ++ (fName l)
 
-	
 
+--depRetriever :: Deps -> IO ()
+depRetriever deps = do
+  mapM_ downLoad (map (\d -> makeURL d) deps)
 
+  
+-- http://mvnrepository.com/artifact/io.fabric8/fabric8-arquillian/2.2.38
+--http://central.maven.org/maven2/io/fabric8/fabric8-arquillian/2.2.38/fabric8-arquillian-2.2.38.jar
 
---
+++ src/Net.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wall #-}
+module Net where
+
+import qualified Data.ByteString as B
+import Network.HTTP
+import Network.URI (parseURI)
+
+
+downLoad :: String -> String -> IO ()
+downLoad name u = do
+  f <- get u
+  B.writeFile name f
+ where
+    get url = let uri = case parseURI url of
+                          Nothing -> error $ "Invalid URI: " ++ url
+                          Just u -> u in
+              simpleHTTP (defaultGETRequest_ uri) >>= getResponseBody
+
