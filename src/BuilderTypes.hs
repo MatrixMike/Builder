a1d745da32aa1c948d65c8bfa828deffc25e7684
mike-k-houghton <mike_k_houghton@yahoo.co.uk>
2016-03-06 22:48:10 +0000

    added itemByName function

--- src/BuilderTypes.hs
+++ src/BuilderTypes.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults -fno-warn-unused-do-bind #-}
-
+{-# LANGUAGE ScopedTypeVariables #-}
 module BuilderTypes where
 
 
@@ -16,3 +16,10 @@ data Build = Build [Module] deriving (Show)
 data Item = Item (Name, Value) deriving (Show)
 data Module = Module {items :: [Item], deps :: Deps}  deriving (Show)
 data Project = Project {env :: Env, build :: Build, deploy :: Deploy} deriving (Show)
+
+itemByName :: Name -> [Item] -> Maybe Item
+itemByName _ [] = Nothing
+itemByName nm (x:xs)
+ | nm == n = Just x
+ | otherwise = itemByName nm xs 
+ where Item (n, _) = x
