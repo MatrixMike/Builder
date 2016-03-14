3d14aa5538e06f19281dbbfcbb004d87983b7059
mike-k-houghton <mike_k_houghton@yahoo.co.uk>
2016-03-14 22:26:35 +0000

    added process

---
+++ src/Process.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults -fno-warn-unused-do-bind #-}
+
+module Process where
+import System.Process
+
+main = do
+	system "java -version"
